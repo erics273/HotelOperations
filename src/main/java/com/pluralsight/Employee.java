1bffa72c89bceda3cf3b724f356e1f98c18db9b2
Eric Schwartz <erics273@gmail.com>
2025-10-21 10:16:29 -0400

    Finished Employee

---
+++ src/main/java/com/pluralsight/Employee.java
@@ -0,0 +1,94 @@
+package com.pluralsight;
+
+public class Employee {
+
+    //class attributes
+    private int employeeId;
+    private String name;
+    private String department;
+    private double payRate;
+    private int hoursWorked;
+
+    //the constructor to help us create an employee
+    public Employee(int employeeId, String name, String department, double payRate, int hoursWorked) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.department = department;
+        this.payRate = payRate;
+        this.hoursWorked = hoursWorked;
+    }
+
+    //getters and setters
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
+
+    public int getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(int hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    //derived getters
+    public double getTotalPay() {
+
+        //calculate regular and overtime pay assuming 1.5 the pay rate for overtime.
+        //that was not in the instructions
+        double regularPay = this.getRegularHours() * this.getPayRate();
+        double overTimePay = this. getOvertimeHours() * this.getPayRate() * 1.5;
+
+        return regularPay + overTimePay;
+    }
+
+    public int getRegularHours() {
+
+//        if( this.getHoursWorked() > 40){
+//            return 40;
+//        }
+//
+//        return this.getHoursWorked();
+
+        return this.getHoursWorked() > 40 ? 40 : this.getHoursWorked();
+    }
+
+    public int getOvertimeHours() {
+//        if( this.getHoursWorked() > 40){
+//            return this.getHoursWorked() - 40;
+//        }
+//
+//        return 0
+
+        return this.getHoursWorked() > 40 ? this.getHoursWorked() - 40 : 0;
+    }
+
+}
--- src/main/java/com/pluralsight/TheApp.java
+++ src/main/java/com/pluralsight/TheApp.java
@@ -9,6 +9,13 @@ public class TheApp {
         //is it available?
         System.out.println("is the room available:" + potatoRoom.isAvailable());
 
+        //if statement similar to above
+        if(potatoRoom.isAvailable()){
+            System.out.println("Yaya the room is available");
+        }else{
+            System.out.println("sucks for you, no room available");
+        }
+
         //change property to make the room not available
         potatoRoom.setDirty(true);
 
@@ -40,5 +47,29 @@ public class TheApp {
         //check the reservation total
         System.out.println(pototaReservation.getReservationTotal());
 
+
+        //test the employee
+        Employee potatoEmployee = new Employee(1, "Eric", "Front Desk", 10.00, 40);
+
+        //print out regular hours
+        System.out.println(potatoEmployee.getHoursWorked());
+
+        //print out OT hours
+        System.out.println(potatoEmployee.getOvertimeHours());
+
+        //print total pay
+        System.out.println(potatoEmployee.getTotalPay());
+
+        //add another hour so there is overtime
+        potatoEmployee.setHoursWorked(41);
+
+        //print out regular hours
+        System.out.println(potatoEmployee.getHoursWorked());
+
+        //print out OT hours
+        System.out.println(potatoEmployee.getOvertimeHours());
+
+        //print total pay
+        System.out.println(potatoEmployee.getTotalPay());
     }
 }
